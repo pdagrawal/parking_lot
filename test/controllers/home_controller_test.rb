30900fbd42bba0aea8e59ecdb2fd04a8cafbafd8
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-12-14 00:39:50 +0530

    Added few test cases for parking spot

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test "should get home page" do
+    get parking_spots_url
     assert_response :success
   end
 
+  test "should get vehicles page" do
+    get vehicles_url
+    assert_response :success
+  end
+
+  test "should get parking history page" do
+    get parkings_url
+    assert_response :success
+  end
 end
--- test/models/parking_spot_test.rb
+++ test/models/parking_spot_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ParkingSpotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "spot type should be either bike or car" do
+    parking_spot = ParkingSpot.new(name: 'Test name', spot_type: 'Bus')
+    assert_not parking_spot.save
+  end
 end
